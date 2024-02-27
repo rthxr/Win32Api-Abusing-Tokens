15e7d37995e21016e2e9697c4e9ff0aaaafc4ac4
arthur <158314381+rthxr@users.noreply.github.com>
2024-02-27 15:32:32 -0300

    adding functions for token extraction

--- TokenEnum/enum.cpp
+++ TokenEnum/enum.cpp
@@ -92,3 +92,78 @@ typedef struct
     wchar_t TokenType[100];
     wchar_t TokenImpersonationLevel[100];
 } TOKEN;
+
+using fNtQuerySystemInformation = NTSTATUS(WINAPI *)(
+    ULONG SystemInformationClass,
+    PVOID SystemInformation,
+    ULONG SystemInformationLenght,
+    PULONG ReturnLenght
+);
+
+void get_token_owner_info(TOKEN *TOKEN_INFO)
+{
+    wchar_t username[MAX_USERNAME_LENGHT];
+    wchar_t domain[MAX_DOMAINNAME_LENGHT];
+    wchar_t full_name[FULL_NAME_LENGHT];
+    SID_NAME_USE sid;
+    DWORD user_lenght = sizeof(username), domain_lenght = sizeof(domain), token_info;
+    if(!GetTokenInformation(TOKEN_INFO->token_handle, TokenOwner, NULL, 0, &token_info))
+    {
+        PTOKEN_OWNER TokenStatisticsInformation = (PTOKEN_OWNER)GlobalAlloc(GPTR, token_info);
+        if(GetTokenInformation(TOKEN_INFO->token_handle, TokenOwner, TokenStatisticsInformation, token_info, &token_info))
+        {
+            LookupAccountSidW(NULL, ((TOKEN_OWNER *)TokenStatisticsInformation)->Owner, username, &user_lenght, domain. &domain_lenght, &sid);
+            _snwprintf_s(full_name, FULL_NAME_LENGHT, L"%ws/%ws", domain, username);
+            wcscpy_s(TOKEN_INFO->owner_name, TOKEN_TYPE_LENGHT, full_name);
+        }
+    }
+}
+
+void get_token_user_info(TOKEN *TOKEN_INFO)
+{
+    wchar_t username[MAX_USERNAME_LENGHT];
+    wchar_t domain[MAX_DOMAINNAME_LENGHT];
+    wchar_t full_name[FULL_NAME_LENGHT];
+    SID_NAME_USE sid;
+    DWORD user_lenght = sizeof(username), domain_lenght = sizeof(domain), token_info;
+
+    if(!GetTokenInformation(TOKEN_INFO->token_handle, TokenUser, NULL, 0, &token_info))
+    {
+        PTOKEN_USER TokenStatisticsInformation = (PTOKEN_USER)GlobalAlloc(GPTR, token_info);
+        if(GetTokenInformation(TOKEN_INFO->token_handle, TokenUser, TokenStatisticsInformation, token_info, &token_info)) 
+        {
+            LookupAccountSidW(NULL, ((TOKEN_USER *)TokenStatisticsInformation)->User.Sid, username, &user_lenght, domain, &domain_lenght, &sid);
+            _snwprintf_s(full_name, FULL_NAME_LENGHT, L"%ws/%ws", domain, username);
+            wcscpy_s(TOKEN_INFO->user_name, TOKEN_TYPE_LENGHT, full_name);
+        }
+    }
+}
+
+void get_token_security_context(TOKEN *TOKEN_INFO)
+{
+    DWORD returned_tokimp_lenght;
+    if(!GetTokenInformation(TOKEN_INFO->token_handle, TokenImpersonationLevel, NULL, 0, &returned_tokimp_lenght))
+    {
+        PSECURITY_IMPERSONATION_LEVEL TokenImpersonationInformation = (PSECURITY_IMPERSONATION_LEVEL)GlobalAlloc(GPTR, returned_tokimp_lenght);
+        if(GetTokenInformation(TOKEN_INFO->token_handle, TokenImpersonationLevel, TokenImpersonationInformation, returned_tokimp_lenght, &returned_tokimp_lenght))
+        {
+            if(*((SECURITY_IMPERSONATION_LEVEL *)TokenImpersonationInformation) == SecurityImpersonation)
+            { wcscpy_s(TOKEN_INFO->TokenImpersonationLevel, TOKEN_TYPE_LENGHT, L"SecurityImpersonation"); }
+            
+            else if (*((SECURITY_IMPERSONATION_LEVEL *)TokenImpersonationInformation) == SecurityDelegation)
+            { wcscpy_s(TOKEN_INFO->TokenImpersonationLevel, TOKEN_TYPE_LENGHT, L"SecurityDelegation"); }
+            
+            else if (*((SECURITY_IMPERSONATION_LEVEL *)TokenImpersonationInformation) == SecurityAnonymous)
+            { wcscpy_s(TOKEN_INFO->TokenImpersonationInformation, TOKEN_TYPE_LENGHT, L"SecurityAnonymous"); }
+            
+            else if (*((SECURITY_IMPERSONATION_LEVEL *)TokenImpersonationInformation) == SecurityIdentification)
+            { wcscpy_s(TOKEN_INFO->TokenImpersonationInformation, TOKEN_TYPE_LENGHT, L"SecurityIdentification"); }
+        }
+    }
+}
+
+
+int wmain(int argc, wchar_t *argv[])
+{
+    return 0;
+}
